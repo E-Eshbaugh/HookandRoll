83802e9da30b379f606d288ca4ed3cbd24ba6be4
Gordon Moore <gordon@gordonmoore.dev>
2025-03-12 20:44:17 -0600

    think i finished push?

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,4 +1,3 @@
-// InventorySlot.cs
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +6,7 @@ using UnityEngine.EventSystems;
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData) {
-        if (transform.childCount == 0) {
-            InventoryItem inventoryItem = eventData.pointerDrag.GetComponent<InventoryItem>();
-            inventoryItem.parentAfterDrag = transform;
-        }
+        Item item = eventData.pointerDrag.GetComponent<Item>();
+        item.parentAfterDrag = transform;
     }
 }
--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -5,26 +5,34 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Item : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [Header("UI")]
-    public Image image;
+    // public Image image;
+    // public Canvas canvas;
 
     [HideInInspector] public Transform parentAfterDrag;
-
     // drag and drop
     public void OnBeginDrag(PointerEventData eventData) {
-        image.raycastTarget = false;
+        // image.raycastTarget = false;
+        Debug.Log("Started Dragging");
         parentAfterDrag = transform.parent;
         transform.SetParent(transform.root);
+        // transform.SetParent(canvas.transform);
+        transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData) {
+        // Debug.Log("Dragging");
+        // Get the current position and print x and y coordinates to Debug log
+        Vector3 position = transform.position;
+        Debug.Log("Position: x = " + position.x + ", y = " + position.y);
         transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData) {
-        image.raycastTarget = true;
+        // image.raycastTarget = true;
+        Debug.Log("Stopped Dragging");
         transform.SetParent(parentAfterDrag);
     }
 }
