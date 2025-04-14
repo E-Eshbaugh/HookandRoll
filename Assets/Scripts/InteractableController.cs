b1147dc156173f7ceb8bd77e7e03648ca55fc1b2
Gabe-Pope22 <gpope@mines.edu>
2025-04-14 01:10:57 -0600

    started with the functionality for the cooking progress bars, also realized my pause menu got swallowed somewhere, so need to re-add that at some point

--- Assets/Scripts/InteractableController.cs
+++ Assets/Scripts/InteractableController.cs
@@ -3,6 +3,10 @@ using TMPro;
 using UnityEngine.SceneManagement;
 public class InteractableController : MonoBehaviour
 {
+    public Transform progressBar;
+    public float timeToFill = 2f;
+    private float timer;
+    private bool isActive;
     public GameObject interactionText;
     public bool playerInRange = false;
     public bool interact = false;
@@ -22,7 +26,14 @@ public class InteractableController : MonoBehaviour
         if (playerInRange && Input.GetKeyDown(KeyCode.I))
         {
             Interact();
+            
         }
+        if (isActive) {
+            timer += Time.deltaTime;
+            float fillAmount = Mathf.Clamp01(timer / timeToFill);
+            progressBar.localScale = new Vector3(fillAmount, 0.25f, 1);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -31,7 +42,8 @@ public class InteractableController : MonoBehaviour
         {
             playerInRange = true;
             interactionText.SetActive(true);
-           
+            isActive = true;
+            timer = 0f;
         }
     }
 
@@ -41,12 +53,15 @@ public class InteractableController : MonoBehaviour
         {
             playerInRange = false;
             interactionText.SetActive(false);
+            isActive = false;
+            timer = 0f;
+            progressBar.localScale = new Vector3(0, 1, 1);
         }
     }
 
     private void Interact()
     {
        interact = true;
-       SceneManager.LoadScene(sceneToLoad);
+       //SceneManager.LoadScene(sceneToLoad);
     }
 }
