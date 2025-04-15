70d0042558904c8c2f34b67a3ca571527b56ff8a
Gabe-Pope22 <gpope@mines.edu>
2025-04-14 19:56:15 -0600

    modified interactions with cooking stations to give item with inventory manager

--- Assets/Scripts/InteractableController.cs
+++ Assets/Scripts/InteractableController.cs
@@ -11,8 +11,7 @@ public class InteractableController : MonoBehaviour
     public bool playerInRange = false;
     public bool interact = false;
     public string sceneToLoad;
-    public Transform inventory;
-    public Item itemToGive;
+    private InventoryManager inventory;
     private bool givenItem;
 
     void Start()
@@ -21,6 +20,7 @@ public class InteractableController : MonoBehaviour
         {
             interactionText.SetActive(false); // Hide text at the start
         }
+        inventory = FindObjectOfType<InventoryManager>();
     }
 
     // Update is called once per frame
@@ -36,12 +36,8 @@ public class InteractableController : MonoBehaviour
             float fillAmount = Mathf.Clamp01(timer / timeToFill);
             progressBar.localScale = new Vector3(fillAmount, 0.25f, 1);
             if (timer >= timeToFill && !givenItem) {
-                if (itemToGive != null) {
-                    giveItem(itemToGive);
-                } else {
-                    Debug.Log("No item to give");
-                    givenItem = true;
-                }
+                GameObject item = Resources.Load<GameObject>("default");
+                inventory.AddItemToInventory(item);
                
             }
         }
@@ -76,21 +72,5 @@ public class InteractableController : MonoBehaviour
     {
        interact = true;
        //SceneManager.LoadScene(sceneToLoad);
-    }
-
-    private void giveItem(Item item) {
-        foreach (Transform slot in inventory) {
-            if (slot.childCount == 0 && !givenItem) {
-                Item newItem = Instantiate(item, slot);
-                newItem.transform.localPosition = Vector2.zero;
-
-                Item itemScript = newItem.GetComponent<Item>();
-                if (itemScript != null) {
-                    itemScript.parentAfterDrag = slot;
-                }
-                givenItem = true;
-            }
-        }
-        Debug.LogWarning("No empty toolbar slots!");
     }
 }
